77fab40c0446e7b533db702e414c748853b5b7e4
Stefan <stefanmeiwald@yahoo.de>
2014-10-28 01:40:16 +0100

    Fixed wrong deployment user

--- after-success.sh
+++ after-success.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 codecov
-ssh -oStrictHostKeyChecking=no deploy@178.62.252.23
+ssh -v -oStrictHostKeyChecking=no deployuser@178.62.252.23
 cd Scala-collaborative-worksheets
 git pull origin master
 ./sbt run
