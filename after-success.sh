b584d70c163059d64c70fd14ddb827ccb0e2ce58
Stefan <stefanmeiwald@yahoo.de>
2014-10-28 08:44:37 +0100

    ssh remote commands

--- after-success.sh
+++ after-success.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 codecov
-ssh -v -oStrictHostKeyChecking=no deployuser@178.62.252.23
-cd Scala-collaborative-worksheets
-git pull origin master
-./sbt run
+ssh -v -oStrictHostKeyChecking=no deployuser@178.62.252.23 "cd Scala-collaborative-worksheets; git pull origin master; ./sbt run"
