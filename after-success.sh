a23ca0629deb6c1fd7f12a18720dde71a1be1b76
Stefan <stefanmeiwald@yahoo.de>
2014-11-21 20:00:11 +0100

    Deploy server with start command

--- after-success.sh
+++ after-success.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 codecov
-ssh -oStrictHostKeyChecking=no deployuser@178.62.252.23 "cd Scala-collaborative-worksheets; git pull origin master; ./sbt clean; ./sbt stage; ./scala-collaborative-worksheets restart; exit $?"
+ssh -oStrictHostKeyChecking=no deployuser@178.62.252.23 "cd Scala-collaborative-worksheets; git pull origin master; ./sbt clean; ./scala-collaborative-worksheets restart; exit $?"
