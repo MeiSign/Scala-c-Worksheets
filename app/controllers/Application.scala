7d710f2ff93d4192eeddc87748be82261a71d4f7
peter-gerhard <peter.gerhard90@gmail.com>
2014-12-04 19:08:30 +0100

    ajax action to interpret action

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import controllers.Interpreter._
 import play.api._
 import play.api.mvc._
 
@@ -8,5 +9,15 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  // Javascript routing
+  def javascriptRoutes = Action {
+    implicit request =>
+    import routes.javascript._
+    Ok(
+      Routes.javascriptRouter("jsRoutes")(
+        Interpreter.interpret
+      )
+    ).as("text/javascript")
+  }
 }
--- app/controllers/Interpreter.scala
+++ app/controllers/Interpreter.scala
@@ -12,38 +12,36 @@ import play.api.Routes
 
 object Interpreter extends Controller {
 
-  private val writer = new java.io.StringWriter
-  private val code = MyHelpers.Code("val x = 1")
-  private val compiler = MyHelpers.Compiler("")
+//  private val writer = new java.io.StringWriter
+//  private val code = Code("val x = 1")
+//  private val compiler = Compiler("")
 
-  def init = Action {
-    Ok(views.html.interpreter(compiler))
+  def index = Action {
+    Ok(views.html.interpreter())
   }
 
-  def interpret = Action { implicit request =>
+  def interpret(input: String) = Action { implicit request =>
+    Ok("Got: " + input)
     // setup interpreter
-    val settings = new Settings
-    settings.usejavacp.value = false
-    settings.deprecation.value = true
-    var imain = new IMain(settings)
-    
-    // interpret code
-    writer.getBuffer.setLength(0)
-    val holder = new MyHelpers.ResultHolder(null)
-    imain.bind("$result__",holder.getClass.getName, holder)
-    val ir = imain.interpret(code.input);
-//    ir match {
-//      case Success => holder.value
-//      case Error => throw new ScriptException("error in: '" + code.input + "'\n" + writer toString)
-//      case Incomplete => throw new ScriptException("incomplete in :'" + code.input + "'\n" + writer toString)
-//    }
-    Ok(views.html.interpreter(compiler))
+    //    val settings = new Settings
+    //    settings.usejavacp.value = false
+    //    settings.deprecation.value = true
+    //    var imain = new IMain(settings)
+    //
+    //    // interpret code
+    //    writer.getBuffer.setLength(0)
+    //    val holder = new MyHelpers.ResultHolder(null)
+    //    imain.bind("$result__",holder.getClass.getName, holder)
+    //    val ir = imain.interpret(code.input);
+    //    ir match {
+    //      case Success => holder.value
+    //      case Error => throw new ScriptException("error in: '" + code.input + "'\n" + writer toString)
+    //      case Incomplete => throw new ScriptException("incomplete in :'" + code.input + "'\n" + writer toString)
+    //    }
+    //    Ok(views.html.interpreter(compiler))
   }
-
 }
 
-object MyHelpers {
-  case class Code(var input: String)
-  case class Compiler(var output: String)
-  case class ResultHolder(var value: Any)
-}
+case class Code(value: String)
+case class Compiler(output: String)
+case class ResultHolder(value: Any)
