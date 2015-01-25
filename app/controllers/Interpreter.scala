5df992a6f09e31c25d2b61957308ff41b2c3a369
peter-gerhard <peter.gerhard90@gmail.com>
2015-01-25 13:11:35 +0100

    interpreter first running

--- app/controllers/Interpreter.scala
+++ app/controllers/Interpreter.scala
@@ -2,24 +2,40 @@ package controllers
 
 import play.api.mvc.{Action, Controller}
 
-import scala.tools.nsc.Settings
-import scala.tools.nsc.interpreter.IMain
+//import javax.script.ScriptEngineManager
+import scala.tools.nsc._
+import scala.tools.nsc.interpreter._
+
+//trait MyType
 
 object Interpreter extends Controller {
+
+//  val interpreter = new ScriptEngineManager().getEngineByName("scala")
+//  val settings = interpreter.asInstanceOf[scala.tools.nsc.interpreter.IMain].settings
+//  settings.embeddedDefaults[Interpreter]
+//  settings.usejavacp.value = true
 
   def index = Action {
     Ok(views.html.interpreter())
   }
 
+//  def interpret(input: String) = Action {
+//    implicit request => interpreter.eval("1 to 10 foreach println")
+//      Ok("Got: " + input)
+//  }
+
   def interpret(input: String) = Action { implicit request =>
     val settings = new Settings
-    settings.usejavacp.value = true
-    settings.deprecation.value = true
+    val scalaLibraryPath = "/home/peter/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.10.3.jar"
+    settings.bootclasspath.append(scalaLibraryPath)
+//    settings.classpath.append(scalaLibraryPath)
+//    settings.embeddedDefaults[MyType]
+//    settings.usejavacp.value = true
+//    settings.deprecation.value = true
     val imain = new IMain(settings)
-    imain.interpret("val x = 1")
-    println(imain.valueOfTerm("x"))
+    imain.interpret("val x = " + input)
     imain.close
-    Ok("Got: " + input)
+    Ok("Res: " + imain.valueOfTerm("x").get)
   }
 
 }
